e4ac30f1b71f712bcb68e2006ffeae29d660dc2c
Anthony Magner <anthony.magner@gmail.com>
2020-09-05 20:21:40 -0600

    asdf

---
+++ bootstrap/bootstrap_testing/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+def create_app():
+  app = Flask(__name__)
+  Bootstrap(app)
+
+  return app
--- build_env.py
+++ build_env.py
@@ -6,6 +6,10 @@ import json
 
 log = logging.getLogger(__name__)
 
+def formatJson(data):
+    print("FormatJson - Data = %s" % str(data))
+    j = json.loads(data)
+    return json.dumps(j,sort_keys=True,indent=4)
 
 def buildCommonConfig():
     common_env = dict()
@@ -18,8 +22,9 @@ def buildCommonConfig():
 
     log.debug("Common Tags: %s" % common_env)
 
-    env = Environment(loader=FileSystemLoader('templates'), trim_blocks=True,lstrip_blocks=True)
-    env.filters['jsonify'] = json.dumps
+    env = Environment(loader=FileSystemLoader('templates'), trim_blocks=True,lstrip_blocks=True,keep_trailing_newline=True)
+    env.filters['jsonify'] = formatJson
+    #env.filters['json_load'] = json.loads
 
 
     template = env.get_template('common.conf.jinja')
@@ -33,9 +38,10 @@ def buildCommonConfig():
 def buildEnvConfig(env_name, scd_data, config_main, config_env, **extra_opts):
     log.info("Building config for %s" % env_name)
     env = Environment(loader=FileSystemLoader('templates'), trim_blocks=True,lstrip_blocks=True)
-    env.filters['jsonify'] = json.dumps
+    env.filters['json_dump'] = json.dumps
+    env.filters['json_load'] = json.loads
 
     template = env.get_template('env.conf.jinja')
-    env_conf = template.render(scd_data=scd_data, host_data=HostList(scd_data))
+    #env_conf = template.render(scd_data=scd_data, host_data=HostList(scd_data))
     #log.debug("Rendered Result:\n%s" % env_conf)
-    return json.loads(env_conf)
+    #return json.loads(env_conf)
